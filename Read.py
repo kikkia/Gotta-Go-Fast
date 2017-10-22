19d6b922f1d6846be6c9b676f2ece547dbec42e7
quade <quade@iastate.edu>
2017-10-22 13:43:07 -0500

    created test code for supcom

--- Read.py
+++ Read.py
@@ -1,6 +1,46 @@
 import cv2
 import numpy as np
-import time 
+import time
+from PIL import ImageGrab
+
+def initRead():
+    global model
+    samples = np.loadtxt('generalsamples.data', np.float32)
+    responses = np.loadtxt('generalresponses.data', np.float32)
+    responses = responses.reshape((responses.size, 1))
+    model = cv2.ml.KNearest_create()
+    model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+
+def ReadValue(startX, startY, endX, endY):
+    im = np.array(ImageGrab.grab(bbox=(startX, startY, endX, endY)))
+
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+    thresh = cv2.threshold(gray, 255, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    ans = ''
+
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 10:
+            [x, y, w, h] = cv2.boundingRect(cnt)
+            if h > 10:
+                cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                roi = thresh[y:y + h, x:x + w]
+                roismall = cv2.resize(roi, (10, 10))
+                roismall = roismall.reshape((1, 100))
+                roismall = np.float32(roismall)
+                retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
+                string = str(int((results[0][0])))
+                if (string == '0' or string == '1' or string == '7'):
+                    if (np.average(dists) < 1200000):
+                        ans += string
+                elif (string == '3' or string == '5'):
+                    if (np.average(dists) < 1600000):
+                        ans += string
+                elif (np.average(dists) < 1400000):
+                    ans += string
+    return ans[::-1]
 
 #######   training part    ###############
 samples = np.loadtxt('generalsamples.data',np.float32)
@@ -13,7 +53,8 @@ model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 ############################# testing part  #########################
 
 start_time = time.time()
-im = cv2.imread('C:/Users/quade/workspace/Gotta-Go-Fast/supcom.jpg')
+im = cv2.imread('C:/Users/quade/workspace/Gotta-Go-Fast/NumberReadTest3.jpg')
+
 out = np.zeros(im.shape,np.uint8)
 gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 #thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
--- SupComCommands.py
+++ SupComCommands.py
@@ -1,7 +1,11 @@
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 import cv2
-import numpy
+import numpy as np
+import time
+from threading import Thread
+from Read import ReadValue
+from Read import initRead
 
 mouse = PyMouse()
 k = PyKeyboard()
@@ -16,6 +20,21 @@ def MultiSelect(startX, startY, endX, endY):
 def SimilarSelect(x, y):
     mouse.click(x, y, 1, 2)
 
+def CommanderSelect():
+    k.press_keys([k.alt_key, ','])
+
+def EnginnerSelect():
+    k.press_keys([k.alt_key, '.'])
+
+def LandFactorySlect():
+    k.press_keys([k.control_key,  k.shift_key, 'l'])
+
+def AirFactorySlect():
+    k.press_keys([k.control_key,  k.shift_key, 'a'])
+
+def SeaFactorySlect():
+    k.press_keys([k.control_key,  k.shift_key, 's'])
+
 def Move(x,y):
     k.tap_key('m')
     mouse.click(x, y)
@@ -25,7 +44,6 @@ def Attack(x, y):
     mouse.click(x, y)
 
 def BuildPower(x,y):
-    k.press_keys([k.alt_key, ','])
     k.tap_key('b')
     k.tap_key('p')
     k.press_keys(k.shift_key)
@@ -33,41 +51,24 @@ def BuildPower(x,y):
     k.release_key(k.shift_key)
     k.tap_key(k.escape_key)
 
-def ReadNumber(upperLeft, lowerRight):
-    processedImage = cv2.imread('C:/Users/quade/workspace/Gotta-Go-Fast/NumberReadTest4.jpg')
-    processedImage = cv2.cvtColor(processedImage, cv2.COLOR_BGR2GRAY)
-    processedImage = cv2.threshold(processedImage, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-    cnts = cv2.findContours(processedImage.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+def ReadPower():
+    ReadValue()
 
-    cnts = cnts[0]
-    digitCnts = []
+def ReadMass():
+    ReadValue()
 
-    # loop over the digit area candidates
-    i = 0
-    j=0
-    for c in cnts:
-        # compute the bounding box of the contour
-        (x, y, w, h) = cv2.boundingRect(c)
+def ReadUnitCap():
+    ReadValue()
 
-        i+= 1
-        # if the contour is sufficiently large, it must be a digit
-        if w >= 1 and (h >= 1 and h <= 100):
-            digitCnts.append(c)
-            j+= 1
-            print(x, '/', y, ':', w, h)
-    print(i)
-    print(j)
-    ctr = numpy.array(digitCnts).reshape((-1, 1, 2)).astype(numpy.int32)
-    blank = cv2.imread("C:/Users/quade/workspace/Gotta-Go-Fast/blank.jpg")
-    cv2.drawContours(blank, [ctr], -1, (0, 255, 0), 3)
-    cv2.imshow('window', processedImage)
-    cv2.imshow('blankwindow', blank)
+def ReadResearch():
+    ReadValue()
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-    # loop over the digit area candidates
-
-ReadNumber(1,1)
+i=0
+j=0
+initRead()
+while True:
+    i+=1
+    j+=1
+    CommanderSelect()
+    BuildPower(i,j)
